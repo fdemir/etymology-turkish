e8b6f951603250c577753d752e03e6030ce3a1f0
Furkan Demir <furkan.demir_@outlook.com>
2020-09-11 04:24:55 +0300

    add footer

---
+++ components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="footer">
+    <div class="footer-top"></div>
+    <div class="footer-bottom"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+  &-top {
+    background: #f1f3f8;
+    height: 300px;
+  }
+  &-bottom {
+    background: #8d93ab;
+    height: 60px;
+  }
+}
+</style>
