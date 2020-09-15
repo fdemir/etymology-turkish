753dd5aab106aefcfc7411fb294fca022de27075
Furkan Demir <furkan.demir_@outlook.com>
2020-09-15 19:14:09 +0300

    add card component

---
+++ components/ui/Card.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+/* stylelint-disable */
+
+.card {
+  border: 0.5px solid #000;
+  box-sizing: border-box;
+  box-shadow: 0px 5px 0px rgba(0, 0, 0, 0.05);
+  border-radius: 6px;
+  padding: 1rem;
+}
+</style>
