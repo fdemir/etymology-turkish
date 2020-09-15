2737deaf433876610379dac0cd03d5f15ae668b3
Furkan Demir <furkan.demir_@outlook.com>
2020-09-15 19:16:45 +0300

    add language card component

---
+++ components/LanguageCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="language-card">
+    <Card> {{ text }} </Card>
+  </div>
+</template>
+
+<script>
+import { Card } from '~/components/ui'
+export default {
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+  components: {
+    Card,
+  },
+}
+</script>
+
+<style lang="scss">
+.language-card .card {
+  box-shadow: 0 5px 0 #2f80ed !important;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+  cursor: pointer;
+}
+</style>
