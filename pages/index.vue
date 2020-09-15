a8b0f7e0a632fd6cac6128a3eb2e49012ca26559
Furkan Demir <furkan.demir_@outlook.com>
2020-09-15 19:17:00 +0300

    add home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,39 @@
 <template>
   <div class="content">
     <SearchSection />
+    <main class="container">
+      <section>
+        <Description />
+      </section>
+      <section>
+        <LanguageOrigins />
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
 import SearchSection from '~/components/Home/SearchSection'
+import Description from '~/components/Home/Description'
+import LanguageOrigins from '~/components/Home/LanguageOrigins'
+
 export default {
   components: {
     SearchSection,
+    Description,
+    LanguageOrigins,
   },
 }
 </script>
 
 <style lang="scss" scoped>
 .content {
-  min-height: 100vh;
+  main {
+    padding: 25px 0;
+
+    section {
+      padding-bottom: 30px;
+    }
+  }
 }
 </style>
