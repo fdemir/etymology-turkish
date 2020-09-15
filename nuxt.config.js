fdfc05c758df73f6b0eb6f0bf083e08be618cf9c
Furkan Demir <furkan.demir_@outlook.com>
2020-09-15 19:15:41 +0300

    add svg loader

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,6 +51,7 @@ export default {
     '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/stylelint-module
     '@nuxtjs/stylelint-module',
+    '@nuxtjs/svg',
   ],
   /*
    ** Nuxt.js modules
