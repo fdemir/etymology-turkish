62d31cc83acac8c3c6f5ecbd7dfd395d9fd74b43
Furkan Demir <furkan.demir_@outlook.com>
2020-09-15 19:16:54 +0300

    add language origins component

---
+++ components/Home/LanguageOrigins.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="language-origins">
+    <h3>Köken Diller</h3>
+    <div class="language-origins__row">
+      <language-card text="Türkçe" />
+      <language-card text="Almanca" />
+      <language-card text="İngilizce" />
+      <language-card text="Kürtçe" />
+    </div>
+  </div>
+</template>
+
+<script>
+import LanguageCard from '~/components/LanguageCard'
+export default {
+  components: {
+    LanguageCard,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.language-origins {
+  &__row {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 15px;
+  }
+}
+</style>
