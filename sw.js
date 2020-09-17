849f2f0c688bc5af88795545c3b0a87dbc87490b
frndmr <48316033+frndmr@users.noreply.github.com>
2020-09-17 22:25:26 +0000

    Deploying to gh-pages from  @ 2429e1e3185db1ff0416cf4de3488105a6160a31 🚀

--- sw.js
+++ sw.js
@@ -28,5 +28,5 @@ workbox.precaching.cleanupOutdatedCaches()
 // --------------------------------------------------
 
 // Register route handlers for runtimeCaching
-workbox.routing.registerRoute(new RegExp('/etymology-turkish/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/etymology-turkish/app/'), new workbox.strategies.CacheFirst ({}), 'GET')
 workbox.routing.registerRoute(new RegExp('/etymology-turkish/'), new workbox.strategies.NetworkFirst ({}), 'GET')
