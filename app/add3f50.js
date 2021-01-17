133ffb4586d324e93771e25fb9b44fe10737f6cc
fdemir <48316033+fdemir@users.noreply.github.com>
2021-01-17 12:32:27 +0000

    Deploying to gh-pages from  @ 0a906a399869e47d6243e6ef942659debc46798a 🚀

--- app/add3f50.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{297:function(n,t,e){"use strict";e.r(t);var c=e(8),component=Object(c.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("Linguistic prescription")])}),[],!1,null,null,null);t.default=component.exports}}]);
---
+++ app/d3e1be9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{294:function(n,t,e){"use strict";e.r(t);var c=e(8),component=Object(c.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("Linguistic prescription")])}),[],!1,null,null,null);t.default=component.exports}}]);
